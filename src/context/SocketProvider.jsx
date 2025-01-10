69918529cfdd8e0100acc73d82bafbb3eb182090
Parth-Manektech <parth.g@manektech.com>
2025-01-11 00:43:41 +0530

    project init

---
+++ src/context/SocketProvider.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useMemo } from "react";
+import io from "socket.io-client"
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+    const socket = useContext(SocketContext);
+    return socket;
+};
+
+export const SocketProvider = (prop) => {
+
+    // const socket = useMemo(() => io('http://192.168.1.26:8000'), []);
+
+    const socket = useMemo(() => io('https://socket-webrtc.vercel.app/api/socket'), []);
+
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {prop.children}
+        </SocketContext.Provider>
+    )
+}
---
+++ src/screens/Lobby.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { useSocket } from "../context/SocketProvider";
+import { useNavigate } from "react-router-dom";
+
+const LobbyScreen = () => {
+    const [email, setEmail] = useState("");
+    const [room, setRoom] = useState("");
+
+    const socket = useSocket();
+    console.log('socket', socket)
+    const navigate = useNavigate();
+
+    const handleSubmitForm = useCallback(
+        (e) => {
+            e.preventDefault();
+            console.log('first', { email, room })
+            socket.emit("room:join", { email, room });
+        },
+        [email, room, socket]
+    );
+
+    const handleJoinRoom = useCallback(
+        (data) => {
+            const { room } = data;
+            navigate(`/room/${room}`);
+        },
+        [navigate]
+    );
+
+    useEffect(() => {
+        socket.on("room:join", handleJoinRoom);
+        return () => {
+            socket.off("room:join", handleJoinRoom);
+        };
+    }, [socket, handleJoinRoom]);
+
+    return (
+        <div>
+            <h1>Lobby</h1>
+            <form onSubmit={handleSubmitForm}>
+                <label htmlFor="email">Email ID</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <label htmlFor="room">Room Number</label>
+                <input
+                    type="text"
+                    id="room"
+                    value={room}
+                    onChange={(e) => setRoom(e.target.value)}
+                />
+                <br />
+                <button>Join</button>
+            </form>
+        </div>
+    );
+};
+
+export default LobbyScreen;
